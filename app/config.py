20411ea26335a23c0aa99fbd46f0a636b96a567a
sebastienjensen <160079243+sebastienjensen@users.noreply.github.com>
2026-01-30 18:31:12 +0000

    Retrieve API token from .env

--- app/config.py
+++ app/config.py
@@ -4,6 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+ROBOTEVENTS_TOKEN = os.getenv("ROBOTEVENTS_TOKEN")
 
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set")
+
+if not ROBOTEVENTS_TOKEN:
+    raise RuntimeError("ROBOTEVENTS_TOKEN is not set")
