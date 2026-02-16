93b069a36bc88b258fc75a86513540dd32d55e36
sebastienjensen <160079243+sebastienjensen@users.noreply.github.com>
2026-02-16 17:33:54 +0000

    Increase pool size

--- app/db.py
+++ app/db.py
@@ -5,7 +5,7 @@ from app.config import DATABASE_URL
 pool = AsyncConnectionPool(
     DATABASE_URL,
     min_size = 1,
-    max_size = 5,
+    max_size = 10,
     timeout = 10,
     kwargs = {"prepare_threshold": 0},
     open = False
